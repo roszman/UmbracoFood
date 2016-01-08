efe4d38467db54e268194c0034ab32670b3a6951
Michał Rocławski <mroclawski@pgs-soft.com>
2016-01-08 16:00:39 +0100

    Added RestaurantRepository with not implemented method with its interface

---
+++ UmbracoFood.Core/Interfaces/IRestaurantsRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UmbracoFood.Core.Models;
+
+namespace UmbracoFood.Core.Interfaces
+{
+    public interface IRestaurantsRepository
+    {
+        void AddRestaurant(Restaurant restaurant);
+
+        void EditRestaurant(Restaurant restaurant);
+
+        void RemoveRestaurant(int id);
+
+        Restaurant GetRestaurant(int id);
+
+        IEnumerable<Restaurant> GetActiveRestaurants();
+
+        IEnumerable<Restaurant> GetInactiveRestaurants();
+    }
+}
---
+++ UmbracoFood.Infrastructure/Repositories/RestaurantRepository.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using UmbracoFood.Core.Interfaces;
+using UmbracoFood.Core.Models;
+
+namespace UmbracoFood.Infrastructure.Repositories
+{
+    public class RestaurantRepository :IRestaurantsRepository
+    {
+        public void AddRestaurant(Restaurant restaurant)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void EditRestaurant(Restaurant restaurant)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveRestaurant(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Restaurant GetRestaurant(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Restaurant> GetActiveRestaurants()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Restaurant> GetInactiveRestaurants()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
