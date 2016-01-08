bcce3c0dad02321c1e71f3ff9c00c9fe87906996
Piotr Polanski <ppolanski@pgs-soft.com>
2016-01-08 11:46:49 +0100

    add Restaurant section/application and corresponding tree

---
+++ UmbracoFood/Custom/Sections/RestaurantsApplication.cs
@@ -0,0 +1,10 @@
+﻿using umbraco.businesslogic;
+using umbraco.interfaces;
+
+namespace UmbracoFood.Custom.Sections
+{
+    [Application("Restaurants", "Restaurants", "icon-food", 99)]
+    public class RestaurantsApplication : IApplication
+    {
+    }
+}
