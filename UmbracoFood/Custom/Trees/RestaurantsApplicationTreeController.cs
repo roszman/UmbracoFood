6196deb24d1f43beb7a5bf218538040a567c6844
Piotr Polanski <ppolanski@pgs-soft.com>
2016-01-08 11:46:49 +0100

    add Restaurant section/application and corresponding tree

---
+++ UmbracoFood/Custom/Trees/RestaurantsApplicationTreeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http.Formatting;
+using umbraco.BusinessLogic.Actions;
+using Umbraco.Web.Models.Trees;
+using Umbraco.Web.Mvc;
+using Umbraco.Web.Trees;
+
+namespace UmbracoFood.Custom.Trees
+{
+    [PluginController("Restaurants")]
+    [Umbraco.Web.Trees.Tree("Restaurants", "RestaurantsTree", "Restauracje")]
+    public class RestaurantsApplicationTreeController : TreeController
+    {
+        protected override TreeNodeCollection GetTreeNodes(string id, FormDataCollection queryStrings)
+        {
+            var nodes = new TreeNodeCollection();
+            var item = this.CreateTreeNode("dashboard", id, queryStrings, "My item", "icon-truck", false);
+            nodes.Add(item);
+            return nodes;
+        }
+
+        protected override MenuItemCollection GetMenuForNode(string id, FormDataCollection queryStrings)
+        {
+            var menu = new MenuItemCollection();
+            menu.DefaultMenuAlias = ActionNew.Instance.Alias;
+            menu.Items.Add<ActionNew>("Create");
+            return menu;
+        }
+    }
+}
