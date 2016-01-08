c2f3d22be8b55f951358d12fa39a64d9f5ad7551
Piotr Polanski <ppolanski@pgs-soft.com>
2016-01-08 10:06:08 +0100

    add IDs to models

--- UmbracoFood.Core/Models/Order.cs
+++ UmbracoFood.Core/Models/Order.cs
@@ -4,6 +4,7 @@ namespace UmbracoFood.Core.Models
 {
     public class Order
     {
+        public long ID { get; set; }
         public IEnumerable<OrderedMeal> OrderedMeals { get; set; }
         public string Owner { get; set; }
         public OrderStatus Status { get; set; }
--- UmbracoFood.Core/Models/OrderedMeal.cs
+++ UmbracoFood.Core/Models/OrderedMeal.cs
@@ -2,6 +2,7 @@
 {
     public class OrderedMeal
     {
+        public long ID { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
         public string PurchaserName { get; set; }
