94f3941377bb8e02019b80532a6d76c16a793d8c
Piotr Polanski <ppolanski@pgs-soft.com>
2016-01-11 14:28:25 +0100

    nie wiem co to, ale dziala

--- UmbracoFood/App_Start/AutofacConfig.cs
+++ UmbracoFood/App_Start/AutofacConfig.cs
@@ -2,6 +2,8 @@
 using System.Web.Mvc;
 using Autofac;
 using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using Umbraco.Web;
 
 namespace UmbracoFood
 {
@@ -16,18 +18,21 @@ namespace UmbracoFood
 
 
             builder.RegisterControllers(typeof (Global).Assembly);
-            
+            builder.RegisterApiControllers(typeof(UmbracoApplication).Assembly);
+            builder.RegisterApiControllers(typeof(Global).Assembly);
+
+
             var container = builder.Build();
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
         }
-        
+
         private static void RegisterServices(ContainerBuilder builder)
         {
             var businessLogic = Assembly.Load("UmbracoFood.Services");
 
             builder.RegisterAssemblyTypes(businessLogic)
                 .Where(t => t.Name.EndsWith("Service"))
-                .AsImplementedInterfaces();            
+                .AsImplementedInterfaces();
         }
 
         private static void RegisterRepositories(ContainerBuilder builder)
