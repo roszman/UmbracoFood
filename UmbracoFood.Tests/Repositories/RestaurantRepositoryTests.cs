b3c08ee5cf25222bdc2fcd135c87220729690505
Michał Rocławski <mroclawski@pgs-soft.com>
2016-01-13 09:01:21 +0100

    Added Moq
    Added Xunit
    First unit test

---
+++ UmbracoFood.Tests/Repositories/RestaurantRepositoryTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace UmbracoFood.Tests.Repositories
+{
+    public class RestaurantRepositoryTests
+    {
+        [Fact]
+        public void AddRestaurant()
+        {
+            Assert.Equal(4, 4);
+        }
+    }
+}
