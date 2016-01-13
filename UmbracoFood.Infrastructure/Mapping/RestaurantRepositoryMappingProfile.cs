6ae3a9a4ac45e8bc618322706b1014f79304d04c
Michał Rocławski <mroclawski@pgs-soft.com>
2016-01-13 11:25:29 +0100

    First unit tests for restaurant repository

--- UmbracoFood.Infrastructure/Mapping/RestaurantRepositoryMappingProfile.cs
+++ UmbracoFood.Infrastructure/Mapping/RestaurantRepositoryMappingProfile.cs
@@ -20,7 +20,8 @@ namespace UmbracoFood.Infrastructure.Mapping
                 .ForMember(d => d.MenuUrl, s => s.MapFrom(o => o.MenuUrl))
                 .ForMember(d => d.Name, s => s.MapFrom(o => o.Name))
                 .ForMember(d => d.Phone, s => s.MapFrom(o => o.Phone))
-                .ForMember(d => d.Url, s => s.MapFrom(o => o.WebsiteUrl));
+                .ForMember(d => d.Url, s => s.MapFrom(o => o.WebsiteUrl))
+                .ForMember(d => d.Active, s => s.Ignore());
 
         }
     }
