9271ce278cc7667f9e4da509917f5e03d66599d1
Michał Rocławski <mroclawski@pgs-soft.com>
2016-01-15 13:11:12 +0100

    angular utilService
    global ajax error interceptor with growl error

--- UmbracoFood/Scripts/angular/umbracoFoodApp.js
+++ UmbracoFood/Scripts/angular/umbracoFoodApp.js
@@ -1,6 +1,19 @@
 ﻿var umbracoFood = angular.module("umbracoFoodApp", ['angular-growl']);
 
 
-umbracoFood.config(['growlProvider', function (growlProvider) {
+umbracoFood.config(['growlProvider', '$httpProvider', function (growlProvider, $httpProvider) {
+
     growlProvider.globalPosition('top-center');
+
+    $httpProvider.interceptors.push(function($q,growl) {
+        return {
+            'responseError': function (response) {
+                var message =  "Operacja nie udała się. Spróbuj ponownie lub skontaktuj się z administratorem.";
+                growl.error(message);
+
+                return $q.reject();
+
+            }
+        };
+    });
 }]);
