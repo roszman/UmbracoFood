c20b2a9a7d26949ff20a8eed6177e06f39c4eeaf
roszman <roszman@gmail.com>
2016-01-15 12:33:34 +0100

    IsActive for ResturantRepositoryMappingProfile and RestaurantRepositoryProfileTests

--- UmbracoFood.Infrastructure/Mapping/RestaurantRepositoryMappingProfile.cs
+++ UmbracoFood.Infrastructure/Mapping/RestaurantRepositoryMappingProfile.cs
@@ -22,7 +22,7 @@ namespace UmbracoFood.Infrastructure.Mapping
                 .ForMember(d => d.Name, s => s.MapFrom(o => o.Name))
                 .ForMember(d => d.Phone, s => s.MapFrom(o => o.Phone))
                 .ForMember(d => d.Url, s => s.MapFrom(o => o.WebsiteUrl))
-                .ForMember(d => d.Active, s => s.Ignore());
+                .ForMember(d => d.IsActive, s => s.Ignore());
 
         }
     }
